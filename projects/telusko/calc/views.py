66e5f6c4ac13d3d9424f351194c26aa929d1b0c6
Your Name <you@example.com>
2020-03-02 00:44:28 +0200

    release v1

---
+++ projects/telusko/calc/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html',{'name':'Igor'})
+
+def add(request):
+
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+
+    return  render(request, 'results.html', {'results':res})
+
+
+
+
